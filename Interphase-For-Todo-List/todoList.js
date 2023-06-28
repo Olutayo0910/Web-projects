5de0478698b09bd201c8b6fe00adb6fadae1d919
Olutayo <olutayoogunlade2022@gmail.com>
2023-06-28 18:06:08 +0100

    update code.

--- Interphase-For-Todo-List/todoList.js
+++ Interphase-For-Todo-List/todoList.js
@@ -1,18 +1,32 @@
-const todoList = ['clean', 'eat'];
+const todoList = [{
+    list: 'clean',
+    dueDate: '2023-07-06'
+},
+{
+    list: 'cleanTay',
+    dueDate: '2023-08-06' 
+    
+}];
 
 function todoHTML()
 {
 let todoListHTML = '';
     for (let i = 0; i < todoList.length; i++)
     {
-        const list = todoList[i];
-        let html = `<p>
-        ${list}
-        <button onclick="
-        todoList.splice(${i}, 1);
-        todoHTML();
-        ">Delete</button>
-        </p>`;
+        const listDateObject = todoList[i];
+        const list = listDateObject.list;
+        const dueDate = listDateObject.dueDate;
+
+        let html = `
+        <div class="objects">
+            <div>${list}</div> 
+            <div>${dueDate}</div>
+            <button onclick="
+                todoList.splice(${i}, 1);
+                todoHTML();
+                " class="delete-todo">Delete</button>
+        </div>
+            `;
         todoListHTML += html;
     }   
 document.querySelector(`.js-output-name`).innerHTML = todoListHTML;
@@ -23,7 +37,13 @@ function todoInput()
 const inputElement = document.querySelector(`.js-input-name`);
 const todoName = inputElement.value;
 
-todoList.push(todoName);
+const inputdueDate = document.querySelector(`.js-input-date`);
+const dueDate = inputdueDate.value;
+
+todoList.push({
+    list: todoName,
+    dueDate: dueDate
+});
 
 console.log(todoList);
 
