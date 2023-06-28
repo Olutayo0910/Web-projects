8e3208ce911ffeff69464a0e0dde8c10783169c1
Olutayo <olutayoogunlade2022@gmail.com>
2023-06-28 15:44:43 +0100

    update code

--- Interphase-For-Todo-List/todoList.js
+++ Interphase-For-Todo-List/todoList.js
@@ -1,5 +1,17 @@
 const todoList = [];
 
+function todoHTML()
+{
+let todoListHTML = '';
+    for (let i = 0; i < todoList.length; i++)
+    {
+        const list = todoList[i];
+        let html = `<p>${list}</p>`;
+        todoListHTML += html;
+    }   
+document.querySelector(`.js-output-name`).innerHTML = todoListHTML;
+}
+
 function todoInput()
 {
 const inputElement = document.querySelector(`.js-input-name`);
@@ -10,4 +22,5 @@ todoList.push(todoName);
 console.log(todoList);
 
 inputElement.value = '';
+todoHTML();
 }
